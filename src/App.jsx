ec24853eb0ca46348216f4cac8deeb91af7ddcd9
Lucas Rocha <lucas.voriarocha@gmail.com>
2024-05-14 20:21:19 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
+import SignInClass from './pages/SignInClass'
 import { SignUp } from './pages/SignUp'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Route path='/' element={<Home />} ></Route>
             <Route path='/signin' element={<SignIn />} ></Route>
             <Route path='/signup' element={<SignUp />} ></Route>
+            <Route path='/signin-class' element={<SignInClass />} ></Route>
         </Routes>
     </BrowserRouter>
   )
